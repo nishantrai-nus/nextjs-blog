5a4bf257cacd378f608e62c81e2c52521637b502
nishantrai-nus <e0560182@u.nus.edu>
2022-07-20 19:30:24 +0800

    add css styling

--- src/index.js
+++ src/index.js
@@ -93,7 +93,9 @@ class Game extends React.Component {
         'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button
+          className="button"
+          onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
     });
